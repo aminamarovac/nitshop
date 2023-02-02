d4a1bc048fa7985715dd78c374d4a5cedd6f8a7c
aminamarovac <aminamarovac@gmail.com>
2023-02-02 17:19:09 +0100

    grid

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,35 @@ import "./App.css";
 import CustomForm from "./Components/CustomForm/CustomForm";
 import CustomDiv from "./Components/CustomDiv/CustomDiv";
 import ToDo from "./Components/ToDo/ToDo";
+import Grid  from "@mui/material";
+import GridCmp from "./Components/Grid/Grid";
 
 const App = () => {
-  const [fullName, setFullName] = useState("");
+  // const [fullName, setFullName] = useState("");
 
-  function dataGrabber(name, lastName) {
-    let fullName = name + " " + lastName;
-    console.log(fullName);
-    setFullName(fullName);
-  }
+  // function dataGrabber(name, lastName) {
+  //   let fullName = name + " " + lastName;
+  //   console.log(fullName);
+  //   setFullName(fullName);
+  // }
 
-  return (
-      <>
-    <div className='App'>
-      <header className='App-header'>
-        <CustomDiv>
-          <h4>{fullName}</h4>
-          <p></p>
-        </CustomDiv>
-        <CustomForm sendData={(name, lName) => dataGrabber(name, lName)} />
-      </header>
-    </div>
-      <ToDo />
-    </>
-  );
-};
-
+//   return (
+//       <>
+//     <div className='App'>
+//       <header className='App-header'>
+//         <CustomDiv>
+//           <h4>{fullName}</h4>
+//           <p></p>
+//         </CustomDiv>
+//         <CustomForm sendData={(name, lName) => dataGrabber(name, lName)} />
+//       </header>
+//     </div>
+//       <ToDo />
+//     </>
+//   );
+// };
+return (
+  <GridCmp/>
+)
+}
 export default App; 
-
