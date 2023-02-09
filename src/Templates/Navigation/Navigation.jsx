2db3523c7b4ee19fecd1a4e3a089d5e657d94f2d
aminamarovac <aminamarovac@gmail.com>
2023-02-09 17:48:59 +0100

    app

--- src/Templates/Navigation/Navigation.jsx
+++ src/Templates/Navigation/Navigation.jsx
@@ -1,21 +1,15 @@
 import React from "react";
-import { Grid } from "@mui/material";
+import {Grid}  from "@mui/material";
 import CustomDiv from "../../Components/CustomDiv/CustomDiv";
 
 const Navigation=()=>{
     return(
         <Grid container direction="row">
-            <Grid item xs={7} sm={5} md={5} lg={4}>
-            <CustomDiv border="2px solid black" width="100%"></CustomDiv>
-            </Grid>
-            <Grid item xs={3} sm={5} md={5} lg={4}>
-            <CustomDiv border="2px solid black" width="100%"></CustomDiv>
-            </Grid>
-            <Grid item xs={2} sm={2} md={2} lg={4}>
-            <CustomDiv border="2px solid black" width="100%"></CustomDiv>
-            </Grid>
+        <CustomDiv padding="0px 20px" border="1px solid black" width="100%">
+            <Grid container direction="row"></Grid>
+        </CustomDiv>
         </Grid>
     )
-}
+};
 
 export default Navigation;
---
+++ src/Templates/Navigation/TopRow.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Grid } from "@mui/material";
+
+const TopRow=()=>{
+    return<Grid container direction="row">
+
+    </Grid>
+}
+
+export default TopRow;
