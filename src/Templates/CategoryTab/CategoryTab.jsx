d47f00ddd02b6d6cda6197e5efed448252605616
aminamarovac <aminamarovac@gmail.com>
2023-03-08 21:00:08 +0100

    app

--- src/Templates/CategoryTab/CategoryTab.jsx
+++ src/Templates/CategoryTab/CategoryTab.jsx
@@ -2,19 +2,106 @@ import React from "react";
 import SimplifiedDiv from "../../Components/SimplifiedDiv/SimplifiedDiv";
 import Text from "../../Components/Text/Text";
 import { colors } from "../../util/theme";
+import AddIcon from "@mui/icons-material/Add";
 
+
+
+const categories = [
+  {
+    name: "Sportswear",
+    active: false,
+  },
+  {
+    name: "Mens",
+    active: false,
+  },
+  {
+    name: "Womens",
+    active: false,
+  },
+  {
+    name: "Kids",
+    active: false,
+  },
+  {
+    name: "Fashion",
+    active: false,
+  },
+  {
+    name: "Households",
+    active: false,
+  },
+  {
+    name: "Interiors",
+    active: false,
+  },
+  {
+    name: "Clothing",
+    active: false,
+  },
+  {
+    name: "Bags",
+    active: false,
+  },
+  {
+    name: "Shoes",
+    active: false,
+  }
+];
+
+const subCategory=[
+  {
+    categoryName:"Sportswear",
+    name:"Adidas",
+    active:false
+  },
+  {
+    categoryName:"Sportswear",
+    name:"Nike",
+    active:false
+  },
+  {
+    categoryName:"Sportswear",
+    name:"Puma",
+    active:false
+  },
+  {
+    categoryName:"Mens",
+    name:"Adidas",
+    active:false
+  },
+  {
+    categoryName:"Sportswear",
+    name:"Nike",
+    active:false
+  },
+  {
+    categoryName:"Sportswear",
+    name:"Puma",
+    active:false
+  },
+]
 const CategoryTab = () => {
   const styles = {
     container: {
       width: "100%",
-      borderWidth: "0.5px",
-      borderColor: colors.lightGray,
-      borderRadius: "5px",
+      border: `0.5px solid ${colors.lightGray} `,
+      borderRadius: "2px",
       textAlign: "left",
+      padding:"10px 20px"
     },
+    category:{
+      padding:"5px 0px"
+    }
   };
 
-  return <SimplifiedDiv style={styles.container}></SimplifiedDiv>;
+
+
+  return <SimplifiedDiv style={styles.container}>
+    {categories.map(cat=>{
+      return(<Text style={styles.category}>{cat.name}</Text>);
+    })}
+  </SimplifiedDiv>;
 };
 
 export default CategoryTab;
